8a4406e2a1fb30dd3886cd731084b1f247e7fb80
maherapi <maher.azkoul@gmail.com>
2020-08-29 01:00:40 +0300

    refactor, add user point context for state management

--- src/components/user-point/User-point.js
+++ src/components/user-point/User-point.js
@@ -1,70 +1,91 @@
-import React, { useState, useEffect} from "react";
-import {Avatar, ConfigProvider, Dropdown, Layout, Menu} from "antd";
-import {BrowserRouter, NavLink as Link, Route, Switch, useLocation} from "react-router-dom";
-import {HomeOutlined,CreditCardOutlined,RiseOutlined,UserOutlined} from '@ant-design/icons';
-import Shipments from './Shipments';
-import PointHome from './point-home';
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import {
+  BrowserRouter,
+  NavLink as Link,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+import {
+  HomeOutlined,
+  CreditCardOutlined,
+  RiseOutlined,
+  UserOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
+import Shipments from "./Shipments";
+import PointHome from "./point-home";
 import Confirmation from "./Confirmation";
 import RecievePage from "./recieve/RecievePage";
+import UserPointState from "../../context/user-point/userPointState";
+import { useContext } from "react";
+import UserPointContext from "../../context/user-point/userPointContext";
+import { colors } from "../../styles/global";
 
-const { Header, Footer, Content } = Layout;
+const { Footer } = Layout;
 
 const UserPointIndex = (props) => {
-    let location = useLocation();
-    let [selected, setSelected] = useState([location.pathname]);
-    const handleClick = e => {
-        console.log('click ', e);
-    };
+  let location = useLocation();
+  let [selected, setSelected] = useState([location.pathname]);
 
-    return (
-        <BrowserRouter>
-            <Layout style={{minHeight: "100vh", paddingBottom: "52px"}}>
-                <Switch>
-                    <Route exact={true} path="/" component={() => <PointHome/>} />
-                    <Route path="/recieve" exact={true} component={() => <RecievePage/>} />
-                    <Route path="/Shipments" exact={true} component={() => <Shipments/>} />
-                    <Route path="/Shipments/:shipment_id" exact={true} component={() => <Confirmation/>} />
-                </Switch>
-            </Layout>
+  return (
+    <UserPointState>
+      <BrowserRouter>
+        <Layout style={{ minHeight: "100vh", paddingBottom: "52px" }}>
+          <Switch>
+            <Route exact path="/" component={PointHome} />
+            <Route path="/recieve" exact component={RecievePage} />
+            <Route path="/Shipments" exact component={Shipments} />
+            <Route
+              path="/Shipments/:shipment_id"
+              exact
+              component={Confirmation}
+            />
+          </Switch>
+        </Layout>
 
-            <Layout style={{position: "fixed", bottom: "0", width: "100vw"}}>
-            <Footer className="webview-footer">
-                <Menu theme="light" mode="horizontal" defaultSelectedKeys={['/']} 
-                    className="flex-container space-between" 
-                    // onClick={handleClick}
-                    onSelect={(selectParam) => {
-                        setSelected(selectParam.keyPath);
-                      }}
-                >
-                    <Menu.Item key="/">
-                        <Link to="/">
-                            <HomeOutlined style={footerIconStyles}/>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="balance">
-                        <Link to="balance">
-                            <CreditCardOutlined style={footerIconStyles}/>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="statistic">
-                        <Link to="statistic">
-                            <RiseOutlined style={footerIconStyles}/>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="profile">
-                        <Link to="profile">
-                            <UserOutlined style={footerIconStyles}/>
-                        </Link>
-                    </Menu.Item>
-                </Menu>
-            </Footer>
-            </Layout>
-        </BrowserRouter>
-    );
+        <Layout style={{ position: "fixed", bottom: "0", width: "100vw" }}>
+          <Footer className="webview-footer">
+            <Menu
+              theme="light"
+              mode="horizontal"
+              defaultSelectedKeys={["/"]}
+              className="flex-container space-between"
+              onSelect={(selectParam) => {
+                setSelected(selectParam.keyPath);
+              }}
+            >
+              <Menu.Item key="/">
+                <Link to="/">
+                  <HomeOutlined style={footerIconStyles} />
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="balance">
+                <Link to="balance">
+                  <CreditCardOutlined style={footerIconStyles} />
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="statistic">
+                <Link to="statistic">
+                  <RiseOutlined style={footerIconStyles} />
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="profile">
+                <Link to="profile">
+                  <UserOutlined style={footerIconStyles} />
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </Footer>
+        </Layout>
+      </BrowserRouter>
+    </UserPointState>
+  );
 };
 
 const footerIconStyles = {
-    margin: "auto"
-}
+  margin: "auto",
+};
 
 export default UserPointIndex;
